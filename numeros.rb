759f74f3b69adadb724409c0b34e9e489165c0e5
Ricardo Alvarez <ri.alvarez.89@icloud.com>
2021-01-10 20:46:57 -0300

    agregado metodo navidad / terminado

--- numeros.rb
+++ numeros.rb
@@ -1,9 +1,9 @@
 n = ARGV[0].to_i
 
 def letra_o(x)
+    space = "\s"
+    character = "*"
     for i in 1..x do
-        space = "\s"
-        character = "*"
         puts character*x if i == 1
         puts character + (space*(x - 2)) + character if i != 1 && i != x
         puts character*x if i == x
@@ -11,9 +11,9 @@ def letra_o(x)
 end
 
 def letra_i(x)
+    space = "\s"
+    character = "*"
     for i in 1..x do
-        space = "\s"
-        character = "*"
         puts character*x if i == 1
         puts (space*(x / 2)) + character if i != 1 && i != x
         puts character*x if i == x
@@ -38,34 +38,77 @@ def letra_x(x)
     character = "*"
     space = "\s" 
     if x.odd?
-        cont = x - 2
+        inter_cont = x - 2
     else
-        cont = ( x - 2 ) + 1
+        inter_cont = ( x - 2 ) + 1
     end
-    sum = 0
+    ext_cont = 0
     x.times do |i|
         if i < (x/2)
-            puts space * sum + character + space * cont + character
-            sum += 1
-            cont -= 2
+            puts space * ext_cont + character + space * inter_cont + character       
+            ext_cont += 1                                                        
+            inter_cont -= 2
         elsif i > (x/2)
-            puts space * sum + character + space * cont + character
-            sum -= 1
-            cont += 2
+            puts space * ext_cont + character + space * inter_cont + character         
+            ext_cont -= 1                                                        
+            inter_cont += 2
         else
-            puts " " * (x / 2) + character
-            sum = x / 2 - 1
-            cont = 1
+            puts space * (x / 2) + character
+            ext_cont = x / 2 - 1                                                   
+            inter_cont = 1
         end
     end
 end
 
-def numero_cero(n)
+def numero_cero(x)
+    character = "*"
+    space = "\s"
+    cont = 0
+    space_size = x-2
 
+
+    puts character*x 
+    (x-2).times do
+        puts character + (space * (cont)) + character + (space * (space_size-1)) + character
+        space_size -= 1
+        cont += 1
+    end
+    puts character*x
 end
 
-letra_o(n)
-letra_i(n)
-letra_z(n)
-letra_x(n)
+def navidad(x)
+    character = "*"
+    space = "\s"
+    cont1 = 0
+    cont2 = 3
+
+    puts space * ((x / 2)+1) + character
+    
+    x.times do |i|
+        if cont1 <= (x / 2)
+            print space * (((x - x / 2) - i) - 1)
+            for i in 1..cont2 do
+                print space if i.even?
+                print character if i.odd?
+            end
+            cont1 += 1
+            cont2 += 2
+            puts
+        else
+            puts space * ((x / 2) + 1) + character
+        end
+    end
+print space
+    for i in 1..x do
+        print space if i.even?
+        print character if i.odd?
+    end
+    puts
+end
+
+# letra_o(n)
+# letra_i(n)
+# letra_z(n)
+# letra_x(n)
 # numero_cero(n)
+navidad(n)
